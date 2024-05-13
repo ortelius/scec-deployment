05a82862e231e7904fb6745c0a3bcf633a9dcaa0
Steve Taylor <steve@deployhub.com>
2024-05-13 11:52:25 -0600

    fix badge for license Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.go
+++ main.go
@@ -162,7 +162,7 @@ func setupRoutes(app *fiber.App) {
 // @version 11.0.0
 // @description RestAPI for the Deployment Object
 // @description ![Release](https://img.shields.io/github/v/release/ortelius/scec-deployment?sort=semver)
-// @description ![license](https://img.shields.io/github/license/ortelius/scec-deployment)
+// @description ![license](https://img.shields.io/github/license/ortelius/.github)
 // @description
 // @description ![Build](https://img.shields.io/github/actions/workflow/status/ortelius/scec-deployment/build-push-chart.yml)
 // @description [![MegaLinter](https://github.com/ortelius/scec-deployment/workflows/MegaLinter/badge.svg?branch=main)](https://github.com/ortelius/scec-deployment/actions?query=workflow%3AMegaLinter+branch%3Amain)
