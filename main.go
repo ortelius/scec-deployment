b30c38bf1b1865fc0e42aa9f5d48ceeaab68313d
Steve Taylor <steve@deployhub.com>
2024-08-05 15:42:19 -0600

    replace InitializeDB with InitializeDatabase Signed-off-by: Steve Taylor <steve@deployhub.com>

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 )
 
 var logger = database.InitLogger()
-var dbconn = database.InitializeDB("evidence")
+var dbconn = database.InitializeDatabase()
 
 // GetDeployments godoc
 // @Summary Get a List of Deployments
